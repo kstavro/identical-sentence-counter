b6e369cafc69e1abbe9a02eebc895e49a37339ea
kstavro <61834522+kstavro@users.noreply.github.com>
2022-02-14 03:00:37 +0100

    Update setup.py
    removed comments

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Author: Konstantinos Stavropoulos <k.stavropoulos@oxolo.com>
+# Author: Konstantinos Stavropoulos <konstantinos.stavrop@gmail.com>
 # For License information, see corresponding LICENSE file.
 
 import os
 
 from setuptools import setup
-
-
-# # from setuptools import find_packages
-
-
-# def package_files(directory):
-#     paths = []
-#     for (path, directories, filenames) in os.walk(directory):
-#         for filename in filenames:
-#             paths.append(os.path.join("..", path, filename))
-#     return paths
-
-
-# # PACKAGES = ["sentence_counter"]
-
-# # PACKAGE_DATA = {
-# #     "sentence_counter": package_files("sentence_counter"),
-# # }
 
 setup(
     name="identical_sentence_counter",
@@ -32,8 +14,6 @@ setup(
     author="Konstantinos Stavropoulos",
     author_email="konstantinos.stavrop@gmail.com",
     licence = "MIT",
-    # packages=PACKAGES,
-    # package_data=PACKAGE_DATA,
     requires_python=">=3.8",
     install_requires=[],
 )
